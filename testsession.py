7dad1c0397b0122e2ef9ea9d3913968a0f57e53b
thinhkb <166593245+thinhkb@users.noreply.github.com>
2025-06-25 16:24:29 +0700

    appoinment

--- testsession.py
+++
@@ -1,7 +0,0 @@
-from appointmentapp import app
-from flask import session, redirect, url_for
-
-@app.route('/login-as/<int:user_id>')
-def login_as(user_id):
-    session['user_id'] = user_id
-    return redirect(url_for('user_bp.profile'))  # nếu dùng blueprint
