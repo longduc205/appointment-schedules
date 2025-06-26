b4fd2047b025e911dae3b78ca1b4e33c92b63563
TongTrungAn <antongtrung19@gmail.com>
2025-06-26 21:42:57 +0700

    login - sign up- login dt - sign up dt - user
    hehehe

--- appointmentapp/__init__.py
+++ appointmentapp/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from appointmentapp.extensions import db
 from appointmentapp.routes_doctor import doctor_bp
-from appointmentapp.routes_signup_login import main_bp  # đổi thành tên blueprint đúng
+from appointmentapp.routes_signup_login import main_bp
 
-app = Flask(__name__)
-app.secret_key = '123456'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/appointmentdb?charset=utf8mb4'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = '123456'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/appointmentdb?charset=utf8mb4'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db.init_app(app)
+    db.init_app(app)
 
-app.register_blueprint(doctor_bp)
-app.register_blueprint(main_bp)
+    app.register_blueprint(doctor_bp)
+    app.register_blueprint(main_bp)
+
+    return app
