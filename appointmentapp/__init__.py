887225bef9000f3a60d65d8036d12784d42da220
longduc205 <longdcuk0205@gmail.com>
2025-06-29 22:46:57 +0700

    {FIX} Sửa Admin không vào được

--- appointmentapp/__init__.py
+++ appointmentapp/__init__.py
@@ -3,6 +3,7 @@ from appointmentapp.extensions import db
 from appointmentapp.routes_doctor import doctor_bp
 from appointmentapp.routes_signup_login import main_bp
 from appointmentapp.user_patient import user_bp
+from appointmentapp.admin import init_admin
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +12,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
+    init_admin(app)
 
     app.register_blueprint(doctor_bp)
     app.register_blueprint(main_bp)
--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from appointmentapp import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    from appointmentapp.admin import *
     app.run(debug=True)
